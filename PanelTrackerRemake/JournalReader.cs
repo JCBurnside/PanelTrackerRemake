b6940c35ac34571f8f8a754433dcc8f361f137ce
James Burnside <jcburnside97@gmail.com>
2018-12-27 12:07:52 -0500

    updated to transfer

---
+++ PanelTrackerRemake/JournalReader.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) James Burnside. All rights reserved.
+// Licensed under the MIT license.
+
+namespace PanelTrackerRemake
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading;
+
+    /// <summary>
+    /// Event type for when the journal updates.
+    /// </summary>
+    /// <param name="line"></param>
+    public delegate void JournalUpdated(string line);
+
+    /// <summary>
+    /// Used to read the journal in a background thread.
+    /// </summary>
+    public class JournalReader
+    {
+        private readonly Regex reg = new Regex("^{.*}$");
+
+        /// <summary>
+        /// Sets the vaProxy to use.
+        /// </summary>
+        public static dynamic VaProxy { set => vaProxy = value; }
+
+        private static dynamic vaProxy;
+
+        /// <summary>
+        /// Fires when ever a new entry is made in the journal.
+        /// </summary>
+        public event JournalUpdated OnJournalUpdate;
+
+        /// <summary>
+        /// Gets a value indicating whether or not the Reader has started.
+        /// </summary>
+        public bool HasStarted => this.hasStarted;
+
+        private Timer timer;
+        private bool hasStarted = false;
+
+        public JournalReader(string[] ignoredEvents)
+        {
+            if(vaProxy)
+            {
+
+            }
+        }
+    }
+}
---
+++ PanelTrackerRemake/PluginMain.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) James Burnside. All rights reserved.
+// Licensed under the MIT license.
+
+namespace PanelTrackerRemake
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+
+    /// <summary>
+    /// Entry point for the plugin.
+    /// </summary>
+    public class PluginMain
+    {
+        /// <summary>
+        /// Auto dected entry point for invocation of this plugin.
+        /// </summary>
+        /// <param name="vaProxy">voice attacks provided type.  unknown type.</param>
+        public static void VA_Invoke1(dynamic vaProxy)
+        {
+            Process[] pname = Process.GetProcessesByName("EliteDangerous64");
+
+            if (!pname.Any())
+            {
+                vaProxy.WriteToLog("Can not proccess.  Elite is not running.", "Red");
+                return;
+            }
+        }
+    }
+}
