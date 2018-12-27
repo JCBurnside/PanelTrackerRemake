9cf2552192a584dba094bcaa1730fb6528aad8f4
James Burnside <jcburnside97@gmail.com>
2018-12-27 18:00:35 -0500

    work done.

--- PanelTrackerRemake/JournalReader.cs
+++ PanelTrackerRemake/JournalReader.cs
@@ -5,6 +5,7 @@ namespace PanelTrackerRemake
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Text;
     using System.Text.RegularExpressions;
     using System.Threading;
@@ -12,27 +13,27 @@ namespace PanelTrackerRemake
     /// <summary>
     /// Event type for when the journal updates.
     /// </summary>
-    /// <param name="line"></param>
-    public delegate void JournalUpdated(string line);
+    /// <param name="line">Line that has parsed into a <see cref="Newtonsoft.Json.Linq.JObject"/>.</param>
+    public delegate void JournalUpdated(Newtonsoft.Json.Linq.JObject line);
 
     /// <summary>
     /// Used to read the journal in a background thread.
     /// </summary>
     public class JournalReader
     {
-        private readonly Regex reg = new Regex("^{.*}$");
-
-        /// <summary>
-        /// Sets the vaProxy to use.
-        /// </summary>
-        public static dynamic VaProxy { set => vaProxy = value; }
-
         private static dynamic vaProxy;
+
+        private readonly Regex reg;
 
         /// <summary>
         /// Fires when ever a new entry is made in the journal.
         /// </summary>
         public event JournalUpdated OnJournalUpdate;
+
+        /// <summary>
+        /// Sets the vaProxy to use.
+        /// </summary>
+        public static dynamic VaProxy { set => vaProxy = value; }
 
         /// <summary>
         /// Gets a value indicating whether or not the Reader has started.
@@ -42,12 +43,32 @@ namespace PanelTrackerRemake
         private Timer timer;
         private bool hasStarted = false;
 
-        public JournalReader(string[] ignoredEvents)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JournalReader"/> class.
+        /// Uses defualt everything.
+        /// </summary>
+        public JournalReader()
+            : this(new[] { "Meusic", "Scan", "Progress", "RefuelAll", "SellExplorationData", "Cargo", "NewCommander" })
         {
-            if(vaProxy)
-            {
+        }
 
-            }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JournalReader"/> class.
+        /// Uses default regex.
+        /// </summary>
+        /// <param name="ignoredEvents"></param>
+        public JournalReader(string[] ignoredEvents)
+            : this(ignoredEvents,Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Saved Games\Frontier Developments\Elite Dangerous")
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ignoredEvents"></param>
+        /// <param name="dir"></param>
+        public JournalReader(string[] ignoredEvents, string dir)
+        {
         }
     }
 }
